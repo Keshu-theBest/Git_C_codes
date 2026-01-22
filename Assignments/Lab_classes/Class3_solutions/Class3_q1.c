0f09509d827c380a5d14bef17dd53fc17f2d642a
Keshu-theBest <gahlotkeshu123@gmail.com>
2026-01-22 13:37:23 +0530

    Adding some more solutions to lab problems

---
+++ Assignments/Lab_classes/Class3_solutions/Class3_q1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(){
+
+    //Taking input and storing it in registers
+    int x;
+    int isPrime=1; //Flag: 1 means Prime and 0 means not prime
+
+    //Taking input and then storing it
+    printf("Please enter the number to check: \n");
+    scanf("%d",&x);
+
+    //Displaying the value the user chose
+    printf("The number you entered was: %d\n",x);
+
+    //Ruling out negative numbers and 0,1 from the condition check
+    if (x<=1)
+    {
+        isPrime=0;
+    }
+    else
+    {
+        //Checking for divisors of x, if any besides 1 and itself
+        //main idea is that if x does not have any divisors upto x/2 (excluding 1), then isPrime remains 1, els becomes 0
+        int a=2;
+        while (a<=x/2)
+        {
+            if (x%a==0)
+            {    isPrime=0;
+                 break;
+            }
+            a++;
+        }
+    }
+
+    //Finally printing the nature of number x as prime or not using isPrime variable
+    if (isPrime==0)
+        printf("The number x is NOT PRIME.\n\n");
+    else if (isPrime==1)
+        printf("The number x is PRIME!!\n\n");
+
+    return 0;
+}
+
---
+++ Assignments/Lab_classes/Class3_solutions/Class3_q2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int A,B,temp;
+    //A and B are our main variables whose value we need to swap
+    //temp is a temporary variable we will b using to store the initial value of one of the main variables to solve the overwrite by swapping.
+
+    //Taking A and B as input and then storing them in two registers say reg_A and reg_B.(That for temp be reg_temp)
+    printf("Please enter A:");
+    scanf("%d",&A);
+    printf("Please enter B:");
+    scanf("%d",&B);
+
+    //printing the initial values of A and B
+    printf("The initial value of A is: %d\n",A);
+    printf("The initial value of B is: %d\n",B);
+
+    temp=B;  //temporarily storing B in reg_temp
+    B=A;     //Overwriting reg_B with the value of A
+    A=temp;  //The initial value of B is in reg_temp so overwriting reg_A with the value in reg_temp
+
+    //Result: A and B are swapped, and temp has the value of initial B
+
+    //printing the final values of A and B
+    printf("\n\nThe value of A after swap: %d\n",A);
+    printf("The value of B after swap: %d\n\n",B);
+
+    //-------------------------------------------------End of Program-------------------------------------------------------//
+
+    return 0;
+}
