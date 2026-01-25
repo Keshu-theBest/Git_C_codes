ad281f2b57fece0179af158017d3074bc4765be9
Keshu-theBest <gahlotkeshu123@gmail.com>
2026-01-25 19:34:38 +0530

    Leap or not

---
+++ Assignments/Lab_classes/Class5_solutions/class5_q1.c
@@ -0,0 +1,45 @@
+//WAP to check for leap year
+
+#include <stdio.h>
+
+void isLeap(int Y)
+{
+    int Leap;
+    if (Y>=0)
+    {
+        if (Y%4==0 && Y%100!=0)
+        {
+            Leap=1;
+        }
+        else if (Y%400==0)
+        {
+            Leap=1;
+        }
+        else
+        {
+            Leap=0;
+        }
+    }
+
+    if (Leap)
+    {
+        printf("\nThe year %d is/was a leap year indeed!!\n\n",Y);
+    }
+    else
+    {
+        printf("\nThe year %d is/was not leap.\n\n",Y)
+    }
+}
+
+int main()
+{
+    int Y;
+    printf("Please enter the Year:\n");
+    scanf("%d",&Y);
+
+    isLeap(Y);
+
+    return 0;
+}
+
+//This I had already done under the 'Trynna build functions' folder earlier, so "ctrl+c and ctrl+v" attack!! 
