687396255048970c3b45d4884817715b1aa0e08c
Keshu-theBest <gahlotkeshu123@gmail.com>
2026-03-02 11:38:41 +0530

    Adding the tasks screenshots to each folder

--- test_fib.c
+++ test_fib.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-void fib(int n){
-    if (n==1 || n==1)
-        return 1;
-    return fib(n-1) + fib(n-2);
+void fib_series(int n){
+    if (n <= 0) return;
+    
+    int first = 0, second = 1;
+    
+    printf("Fibonacci Series: ");
+    for (int i = 0; i < n; i++){
+        if (i == 0)
+            printf("%d ", first);
+        else if (i == 1)
+            printf("%d ", second);
+        else {
+            int next = first + second;
+            printf("%d ", next);
+            first = second;
+            second = next;
+        }
+    }
+    printf("\n");
+}
+
+int main(){
+    int n;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    fib_series(n);
+    return 0;
 }
